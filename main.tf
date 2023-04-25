79d8427adcdef1842d5ffaab26bb8fabdbc06c3b
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-25 20:41:44 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
+
+#  iam_instance_profile {
+#    name = "test"
+#  }
+
+  image_id = data.aws_ami.ami.id
+  instance_type = var.instance_type
+
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      var.tags,
+      {
+        Name = "${var.component}-${var.env}"
+      },
+    )
+  }
+
+#  user_data = filebase64("${path.module}/example.sh")
+}
+
+resource "aws_autoscaling_group" "main" {
+  vpc_zone_identifier = var.subnets
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    propagate_at_launch = false
+    value               = "${var.component}-${var.env}"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "component" {}
+variable "env" {}
+variable "instance_type" {}
+variable "tags" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "vpc_zone_identifier" {}
+variable "subnets" {}
