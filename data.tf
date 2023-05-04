22630ad857bd6ec288aa391ff0c8ebaff68fb549
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-05-04 13:27:52 +0530

    updated

--- data.tf
+++ data.tf
@@ -5,8 +5,10 @@ data "aws_ami" "ami" {
 }
 
 
-data "aws_caller_identity" "domain" {}
-
 data "aws_route53_zone" "domain" {
   name = var.dns_domain
 }
+
+
+data "aws_caller_identity" "account" {}
+
